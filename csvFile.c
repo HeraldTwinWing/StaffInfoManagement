81cc91055f7ca1a1875d269eede49ccd9759d974
HeraldTwinWing <HeraldTwinWing@users.noreply.github.com>
2018-11-20 21:45:05 +0800

    bug found

--- csvFile.c
+++ csvFile.c
@@ -76,7 +76,13 @@ char ***sheetOpen(int openMode)
     {
         char *str = malloc(100 * sizeof(char));
         fgets(str, 100, temp);
-        char *a = strtok(str, ",");
+        char *a = malloc(100 * sizeof(char));
+        for (int l = 0; l < 100; ++l)
+        {
+            *(a + l) = '\0';
+        }
+
+        a = strtok(str, ",");
 
         // 将表格内容存入数组
         for (int k = 0; k < 100; ++k)
@@ -99,7 +105,7 @@ char ***sheetOpen(int openMode)
                 st[j][i][k] = a[k];
             }
         }
-
+        free(a);
         free(str);
     }
 
